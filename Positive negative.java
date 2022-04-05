2c41dfc2ef91c9d2d3e9f16ce6d6f6cfe7eb56f2
Ashutosh-krs <65009386+Ashutosh-krs@users.noreply.github.com>
2022-04-05 17:53:06 +0530

    Create Positive negative.java

---
+++ Positive negative.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public ArrayList<Integer> solve(ArrayList<Integer> A) {
+        int posCnt=0,negCnt=0;
+        for(int i=0;i<A.size();i++){
+            if(A.get(i)>=0)
+                posCnt++;
+            negCnt++;
+        }
+        return new ArrayList<Integer>()
+    }
+}
